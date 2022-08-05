8f1b1deb47e2dd45b27914810b0c47b510741588
Adrian Mitre <mitrehealthcare@gmail.com>
2022-08-05 15:24:59 -0700

    create .env file + PORT

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+require ('dotenv').config())
 const express = require ('express')
 const app = express()
 app.get('/', (req, res)=>{
 res,send('Hola!')
 })
 
-app.listen(3001)
+app.listen(process.env.PORT)
