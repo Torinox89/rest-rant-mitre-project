2e270d799d23dfcd5d388e0d4052d86bf98803c6
Adrian Mitre <mitrehealthcare@gmail.com>
2022-08-12 19:40:38 -0700

    add Places bug solved, and working

--- controllers/places.js
+++ controllers/places.js
@@ -18,24 +18,8 @@ router.get('/:id', (req, res) => {
   }
 })
 
-
-
 // GET /places
-router.get('/', (req, res) => {
-  let places = [{
-    name: 'H-Thai-ML',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
-    pic: '/images/chris-liverani-restaurant-two.jpg'
-  }, {
-    name: 'Coding Cat Cafe',
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: 'Coffee, Bakery',
-    pic: 'images/jay-restaurant-one.jpg'
-  }]
-  
+router.get('/', (req, res) => {   
   res.render('places/index', { places })
 
 })
