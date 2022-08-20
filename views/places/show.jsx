2789dbba36e8e574b6642ed1955c58c56d308779
Adrian Mitre <mitrehealthcare@gmail.com>
2022-08-19 21:46:18 -0700

    show page adjustmen in progress

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -4,37 +4,41 @@ const Def = require('../default')
 function show (data) {
   console.log(data)
     return (
-        <Def>
-          
-          <main>
-            <h1>{ data.place.name }</h1>
-                       
+        <Def>                
+          <main>                                    
+            <body>
+              <div className="row">
+                <div className="col-sm-6">
+                  <img src={data.place.pic} alt={data.place.name} />
+                  <h3> Located in {data.place.city}, {data.place.state}</h3>
+                </div>
+                <div className="col-sm-6">
+                  <h1>{ data.place.name }</h1> 
+                  <h2>Rating</h2>
+                  <h4>Not Rated</h4>
+                  <h2>Description</h2>
+                  <h3>
+                    {data.place.showEstablished()}
+                  </h3>         
+                  <h4>
+                    Serving {data.place.cuisines}
+                  </h4>
+                  <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
+                    Edit
+                  </a>    
+                  <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
+                    <button type="submit" className="btn btn-danger">
+                      Delete
+                    </button>
+                  </form>
+                </div>          
+              </div>
+                  <h2>Comments</h2>
+                  <h4>Not Comments yet</h4>
+            </body>
           </main>
-          <body>
-          <div className="col-sm-6">
-            <h4><img src={data.place.pic} alt={data.place.name} className="place-image"/></h4>
-            </div>
-            <div className="block">
-            <h2>Rating</h2>
-            <h4>Not Rated</h4>
-            <h2>Description</h2>
-            <h4> Located in {data.place.city}, {data.place.state}</h4>
-          </div>
-          <h2>Comments</h2>
-          <h4>Not Comments yet</h4>
-          <div>
-          <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
-            Edit
-          </a>    
-          <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
-          <button type="submit" className="btn btn-danger">
-            Delete
-          </button>
-          </form> 
-             </div>
-          </body>
         </Def>
-    )
-}
+      )
+    }
 
 module.exports = show
