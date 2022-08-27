d90bd7449cfd21df612a7c10e075d274bc05343c
Adrian Mitre <mitrehealthcare@gmail.com>
2022-08-27 09:27:54 -0700

    Comment on Thai is working

--- views/places/comment.jsx
+++ views/places/comment.jsx
@@ -3,53 +3,42 @@ const Def = require('../default.jsx')
 
 function comment () {
 return(
-<def>
-    <main>
-    <h1>Edit Place</h1>
-              <form method="POST" action={`/places/${data.id}?_method=PUT`}>
-              <div className="row">
-                  <div className="form-group col-sm-6">
-                      <label htmlFor="name">Author</label>
-                      <input 
-                          className="form-control" 
-                          id="name" 
-                          name="name" 
-                          value={data.place.name} 
-                      required />
-                  </div>
+  <def>
+        <main>
+          <h1>Edit Comment</h1>
+            <form method="POST" action={`/places/${data.id}?_method=PUT`}>
+              <div className="form-group col-sm-6">
+                    <label htmlFor="author">Author</label>
+                    <input 
+                        className="form-control" 
+                        id="author" 
+                        name="author" 
+                        value={data.comment.author} 
+                    required />
               </div>
-              <div className="row">
-                  <div className="form-group col-sm-6">
-                      <label htmlFor="content">Content</label>
-                      <input className="form-control" id="content" name="content" 
-                      value={data.place.pic}  />
-                     
-                  </div>
+                                
+              <div className="form-group col-sm-6">
+                    <label htmlFor="content">Content</label>
+                    <input className="form-control" id="content" name="content" 
+                    value={data.comment.content}  />
+                  
               </div>
-                <div className="form-group">
-                  <label htmlFor="starRating">Star rating</label>
-                  <input className="form-control" id="starRating" name="starRating" 
-                  value={data.place.city} />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="state">Rant</label>
-                  <input className="form-control" id="state" name="state"
-                  value={data.place.state}  />
-                </div>
-               
-                </div>
-                <input className="btn btn-primary" type="submit" value="Add a Comment" />
-              </form>         
-    </main>
+              <div className="form-group">
+                <label htmlFor="starRating">Star rating</label>
+                <input className="form-control" id="starRating" name="starRating" 
+                value={data.comment.stars} />
+              </div>
+              <div className="form-group">
+                <label htmlFor="rant">Rant</label>
+                <input className="form-control" id="rant" name="rant"
+                value={data.comment.rant}  />
+              </div>                                          
+              <input className="btn btn-primary" type="submit" value="Add a Comment" />
+            </form>         
+        </main>
 
-
-</def>
-
+    </def>
 )
-
-
-
-
 
 }
 
